49779ccfa85720f0082d3a55db4b9bfb9cc25d22
tlsskfk <shin.skfk@gmail.com>
2023-04-26 20:03:06 -0400

    first lambda func

---
+++ lambda/emailToS3.py
@@ -0,0 +1,52 @@
+import boto3
+import requests
+import json
+
+def lambda_handler(event, context):
+    
+    # Extract the email header and attachments from the incoming email
+    message = event['Records'][0]['Sns']['Message']
+    header = message['headers']
+    attachments = message['attachments']
+    
+    # Specify the host and port of the Docker container
+    host = 'my-docker-host.com'
+    port = '8080'
+    
+    # Construct the URL of the Docker container
+    url = f'http://{host}:{port}/myapp'
+    
+    # Send the email header to the Docker container and receive the response
+    response = requests.post(url, data=json.dumps(header))
+    
+    # Check the response and process the attachments if the data is true
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            # Upload each attachment to a separate S3 bucket object
+            s3 = boto3.client('s3')
+            for attachment in attachments:
+                # Extract the attachment name and content
+                name = attachment['name']
+                content = attachment['content']
+                
+                # Upload the attachment to an S3 bucket
+                s3.put_object(Bucket='my-s3-bucket', Key=name, Body=content)
+            
+            # Return a success message if all attachments were uploaded
+            return {
+                'statusCode': 200,
+                'body': 'All attachments were uploaded successfully'
+            }
+        else:
+            # Return a message if there were no attachments to upload
+            return {
+                'statusCode': 200,
+                'body': 'No attachments to upload'
+            }
+    else:
+        # Return an error if the Docker container did not return a valid response
+        return {
+            'statusCode': 500,
+            'body': 'Error: Could not retrieve data from Docker container'
+        }
