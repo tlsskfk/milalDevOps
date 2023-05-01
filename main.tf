4a4f896eaa7415009bed38ce05a2839daa845e36
tlsskfk <shin.skfk@gmail.com>
2023-04-30 23:19:12 -0400

    env troubleshooting

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = process.env.AWS_
+    access_key = "${TF_VAR_AWS_ACCESS_KEY}"
+    secret_key = "${TF_VAR_AWS_SECRET_ACCESS_KEY}"
 }
 
-resource "aws_ami" "milal_master_node" {
+resource "aws_instance" "milal_master_node" {
   ami           = "ami-0557a15b87f6559cf"
   instance_type = "t2.micro"
 }
