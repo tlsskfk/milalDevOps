8984b33c3b00118a0c5fe0e120f424f9e1917b04
tlsskfk <shin.skfk@gmail.com>
2023-04-30 22:33:08 -0400

    ec2 test

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 provider "aws" {
     region = "us-east-1"
+    access_key = process.env.AWS_
+}
+
+resource "aws_ami" "milal_master_node" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
 }
