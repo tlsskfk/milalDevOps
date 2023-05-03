9d272044c32427ed1582dfa5b4cd2a01d0c0d8b8
tlsskfk <shin.skfk@gmail.com>
2023-05-02 20:21:45 -0400

    started terraform

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
+variable "region" {
+    default = "us-east-1"
+}
+
 provider "aws" {
-    region = "us-east-1"
-    access_key = "${TF_VAR_AWS_ACCESS_KEY}"
-    secret_key = "${TF_VAR_AWS_SECRET_ACCESS_KEY}"
+    region = "${var.region}"
 }
 
 resource "aws_instance" "milal_master_node" {
-  ami           = "ami-0557a15b87f6559cf"
-  instance_type = "t2.micro"
+    ami           = "ami-0557a15b87f6559cf"
+    instance_type = "t2.micro"
 }
