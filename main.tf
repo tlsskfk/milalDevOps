ba6f159bf007b3a5fa2771ab88fa847a2da1745b
tlsskfk <shin.skfk@gmail.com>
2023-05-02 22:39:13 -0400

    terraform random files

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
     region = "${var.region}"
 }
 
-resource "aws_instance" "milal_master_node" {
+resource "aws_instance" "milal_cluster" {
     ami           = "ami-0557a15b87f6559cf"
     instance_type = "t2.micro"
 }
