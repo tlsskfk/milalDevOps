362c81f1948ad8c6f8507eb2f5bb57a4bb9d1f0b
tlsskfk <shin.skfk@gmail.com>
2023-05-06 22:55:11 -0400

    terraform ebs

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ variable "region" {
     default = "us-east-1"
 }
 
+variable "availability_zone" {
+    default = "us-east-1a"
+}
+
 provider "aws" {
     region = "${var.region}"
 }
@@ -9,4 +13,10 @@ provider "aws" {
 resource "aws_instance" "milal_cluster" {
     ami           = "ami-0557a15b87f6559cf"
     instance_type = "t2.micro"
+}
+
+resource "aws_ebs_volume" "milal_mysql_volume" {
+  availability_zone = "${var.availability_zone}"
+  size              = 10
+  type              = "gp2"
 }
