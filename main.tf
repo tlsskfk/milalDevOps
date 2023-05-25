bbe1ef0a7959ed596d326150095adf229bfd1e51
Stephen Shin <shin.skfk@gmail.com>
2023-05-25 00:43:42 -0400

    vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+    region = "${var.region}"
+}
+
 variable "region" {
     default = "us-east-1"
 }
@@ -6,10 +10,21 @@ variable "availability_zone" {
     default = "us-east-1a"
 }
 
-provider "aws" {
-    region = "${var.region}"
+variable "" {
+    default = ""
 }
 
+// vpc
+resource "aws_vpc" "milal-vpc" {
+  cidr_block = "10.0.0.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "milal-vpc"
+  }
+}
+
+//k8s cluster ec2
 resource "aws_instance" "milal_cluster" {
     ami           = "ami-0557a15b87f6559cf"
     instance_type = "t2.micro"
@@ -23,6 +38,7 @@ resource "aws_instance" "milal_cluster" {
   }
 }
 
+//k8s mysql container volume
 resource "aws_ebs_volume" "milal_mysql_volume" {
   availability_zone = "${var.availability_zone}"
   size              = 10
